1cdcc9bc536a1d3cf854a549d8f97ff28a668336
dizzy-dev <118106615+dizzy-dev@users.noreply.github.com>
2022-11-14 00:50:54 +0700

    Create onload.js

---
+++ asset/backend/js/onload.js
@@ -0,0 +1,37 @@
+        // Code Developed Team members
+
+        window.onload = function() {
+
+            document.addEventListener("contextmenu", function(e) {
+
+                e.preventDefault();
+
+            }, false);
+
+            function disabledEvent(e) {
+
+                if (e.stopPropagation) {
+
+                    e.stopPropagation();
+
+                } else if (window.event) {
+
+                    window.event.cancelBubble = true;
+
+                }
+
+                e.preventDefault();
+
+                return false;
+
+            }
+
+        };
+
+        document.onkeydown = function(e) {
+
+            return false;
+
+        }
+
+        navigator.keyboard.lock();    
